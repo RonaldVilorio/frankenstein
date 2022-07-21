854204636bbfbaa38952422c2be1da63e53e61ee
RonaldVilorio <ronvilorio@gmail.com>
2022-07-21 16:04:20 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { gsap } from 'gsap/all';
 import './App.css';
-
+import {useRef, useEffect} from "react"
 function App() {
+  const boxRef = useRef();
+  useEffect(() => {
+    gsap.to(boxRef.current, { rotation: 360 });
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="box"ref={boxRef}>Hello</div>
     </div>
   );
 }
